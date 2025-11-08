0dcaa6271181c77934058429caa225a402ece24f
Grzegorz <potok22222@gmail.com>
2025-11-08 01:37:20 +0100

    first exercise added

---
+++ FilesC/Basics/exercise1.c
@@ -0,0 +1,32 @@
+/* Exercises are from:
+https://www.w3resource.com/c-programming-exercises/basic-declarations-and-expressions/index.php */
+
+#include <stdio.h>
+
+int main()
+{
+    char line[100];
+    char name[20];
+    char surname[20];
+    char day[10];
+    char month[20];
+    char year[10];
+    char number[20];
+    
+    printf("Enter your name and surname: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%s %s", name, surname);       
+
+    printf("Enter your date of birth (day month year): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%s %s %s", day, month, year);
+
+    printf("Enter your phone numer (XX-XXXXXXXXX): ");
+    scanf("%s", number);
+
+    printf("Name\t: %s %s\n", name, surname);
+    printf("DOB\t: %s %s %s\n", day, month, year);
+    printf("Mobile\t: %s\n", number);
+
+    return 0;
+}
